43cbf19b2f6b429ed954b3aed6ae4929fca444d7
Lynn Zukerman <lynnzukerman@gmail.com>
2023-01-19 16:02:19 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,124 @@
+let WEATHER_API_KEY = 'bc37c4c6e4e2fd9161be60e50b4ec6a6';
+let GIPHY_API_KEY = 'uPDmIhxOnFGaxw5OyJte2lCjscc1pcAu&s';
+
+const locationSearch = document.querySelector('#locationSearch');
+const moodImg = document.querySelector('#moodImage');
+let imgText = document.querySelector('#imgText');
+let degrees = document.querySelector('.degrees');
+let feelsLike = document.querySelector("#feels-like");
+let wind = document.querySelector('#wind');
+let humidity = document.querySelector('#humidity');
+let weatherDescription = document.querySelector('#weatherDescription');
+let locationText = document.querySelector('#locationText');
+
+locationSearch.addEventListener('click', function() {
+    let searchText = document.querySelector('#location').value;
+    let locationAPIcall = '';
+    //if search is a zip code (begins with a number), call the zip api
+    if (/^\d/.test(searchText)) {
+        locationAPIcall = `http://api.openweathermap.org/geo/1.0/zip?zip=${searchText}&appid=${WEATHER_API_KEY}`
+    }
+    //otherwise call the location api
+    else locationAPIcall = `http://api.openweathermap.org/geo/1.0/direct?q=${searchText}&limit=5&appid=${WEATHER_API_KEY}`;
+    (async function () {
+        const [weatherData, locationData, weatherError] = await getWeather(locationAPIcall);
+        if (!weatherData) {
+            moodImg.src = weatherError;
+        }
+        let gifSearch = setGiphySearchTerm(weatherData.weather[0].main);
+        const [gifData, gifError] = await getGif(gifSearch);
+        //render gif
+        moodImg.src = (!gifData) ? gifError : gifData;
+        imgText.textContent = `Randomized search for "${gifSearch}" from giphy.com`;
+        //render all other data
+        renderData(weatherData, locationData);
+    })();
+});
+
+window.addEventListener('load', function() {
+    let locationAPIcall = `http://api.openweathermap.org/geo/1.0/direct?q=Denver&appid=${WEATHER_API_KEY}`;
+    (async function () {
+        const [weatherData, locationData, weatherError] = await getWeather(locationAPIcall);
+        if (!weatherData) {
+            moodImg.src = weatherError;
+        }
+        let gifSearch = setGiphySearchTerm(weatherData.weather[0].main);
+        const [gifData, gifError] = await getGif(gifSearch);
+        //render gif
+        moodImg.src = (!gifData) ? gifError : gifData;
+        imgText.textContent = `Randomized search for "${gifSearch}" from giphy.com`;
+        //render all other data
+        renderData(weatherData, locationData);
+    })();
+})
+
+function renderData(weatherData, locationData) {
+    degrees.textContent = Math.round(weatherData.main.temp);
+    feelsLike.textContent = `Feels like: ${Math.round(weatherData.main.feels_like)}`;
+    wind.textContent = `Wind: ${Math.round(weatherData.wind.speed)} MPH`;
+    humidity.textContent = `Humidity: ${Math.round(weatherData.main.humidity)}%`;
+    weatherDescription.textContent = weatherData.weather[0].description.toUpperCase();
+    locationText.textContent = locationData.state ? `${locationData.name}, ${locationData.state}` : `${locationData.name} - ${locationData.zip}`;
+}
+
+async function getWeather(locationAPIcall) {
+    try {
+        let locationResponse = await fetch(locationAPIcall, {mode: 'cors'});
+        let json = await locationResponse.json();
+        const lat = Array.isArray(json) ? json[0].lat : json.lat;
+        const lon = Array.isArray(json) ? json[0].lon : json.lon;
+        let locationData = Array.isArray(json) ? json[0] : json;
+        let weatherAPIcall = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=imperial`;
+        let weatherResponse = await fetch(weatherAPIcall, {mode: 'cors'});
+        let weatherData = await weatherResponse.json();
+        return [weatherData, locationData, null];
+      } catch {
+          return [null, null, 'error.jpg']
+      }
+}
+
+async function getGif(term) {
+    try {
+        let fetchURL = `https://api.giphy.com/v1/gifs/translate?api_key=${GIPHY_API_KEY}=${term}&weirdness=10`;
+        moodImg.src = '';
+        let response = await fetch(fetchURL, {mode: 'cors'});
+        let json = await response.json();
+        return [json.data.images.original.url, null];
+        }
+      catch {
+        return [null, "https://media.giphy.com/media/RIq4nU3TgUQztUhYRr/giphy.gif"]; 
+      }   
+};
+
+function setGiphySearchTerm(weatherData) {
+    console.log(typeof weatherData);
+    let search = '';
+    switch(weatherData) {
+        case "Clear":
+            search = 'sunny mood';
+            break;
+        case "Thunderstorm":
+            search = 'thunderstorm';
+            break;
+        case "Drizzle":
+            search = 'drizzle';
+            break;
+        case "Rain":
+            search = 'rainy mood';
+            break;
+        case "Snow":
+            search = 'snowstorm';
+            break;
+        case "Atmosphere":
+            search = 'mist';
+            break;
+        case "Clouds":
+            search = 'cloudy';
+            break;
+        default:
+            search = '404';
+            break;
+    }
+    console.log("search term is " + search);
+    return search;
+}
